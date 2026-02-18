61526eb156e615a4e9b696c90af8fcc14152f976
Daniel <daniel@chainflip.io>
2026-02-18 18:02:00 +0100

    fix: host releases publicly

--- Formula/cf-gov.rb
+++ Formula/cf-gov.rb
@@ -6,22 +6,22 @@ class CfGov < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/chainflip-io/cf-gov-js/releases/download/v#{version}/cf-gov-darwin-arm64"
+      url "https://github.com/chainflip-io/homebrew-tap/releases/download/cf-gov-v#{version}/cf-gov-darwin-arm64"
       sha256 "730b70ff5f6ac9730e4e360f7c92074ba93f49a87d33d8fcb6fcbdae2e724307"
     end
     on_intel do
-      url "https://github.com/chainflip-io/cf-gov-js/releases/download/v#{version}/cf-gov-darwin-x64"
+      url "https://github.com/chainflip-io/homebrew-tap/releases/download/cf-gov-v#{version}/cf-gov-darwin-x64"
       sha256 "324f5f9b06fa888d9a539338135de122435840e92733e28a03cc5b70079b6bcc"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/chainflip-io/cf-gov-js/releases/download/v#{version}/cf-gov-linux-arm64"
+      url "https://github.com/chainflip-io/homebrew-tap/releases/download/cf-gov-v#{version}/cf-gov-linux-arm64"
       sha256 "673d4692c9c9630978cf443c9c85b894add16be45e65a356cbc82ee2fbf468a1"
     end
     on_intel do
-      url "https://github.com/chainflip-io/cf-gov-js/releases/download/v#{version}/cf-gov-linux-x64"
+      url "https://github.com/chainflip-io/homebrew-tap/releases/download/cf-gov-v#{version}/cf-gov-linux-x64"
       sha256 "813e3f2d79cae4c9347e8dbff73901072f3a9f7f96a57c1246d3a6b5f2b83869"
     end
   end
